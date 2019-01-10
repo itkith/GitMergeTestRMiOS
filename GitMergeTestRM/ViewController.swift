a2cf47e2e0487157dd3e1887210954ed53deacd3
Raj Mohan <>
2019-01-10 15:02:26 +0530

    Test Commit

---
+++ GitMergeTestRM/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GitMergeTestRM
+//
+//  Created by Raj Mohan on 2019-01-10.
+//  Copyright © 2019 Raj Mohan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
