c451e03d20582666d0e58d404145a3d6d61fa728
Humbi <Humbhi@humbis-macbook-pro.local>
2019-01-10 15:43:22 +0530

    push

--- GitMergeTestRM/ViewController.swift
+++ GitMergeTestRM/ViewController.swift
@@ -12,7 +12,6 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        testMethod()
         // Do any additional setup after loading the view, typically from a nib.
         setPagelayout()
     }
